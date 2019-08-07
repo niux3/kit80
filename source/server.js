3baa8c9b872d39f71df3d9844148c88b1077e4cb
niuxe <renaudbourdeau@gmail.com>
2019-08-07 22:29:26 +0200

    refacto(server): delete class and add closure

--- source/server.js
+++ source/server.js
@@ -1,142 +1,136 @@
-class Server{
-    constructor(){
-        //dependances
-        let Twig = require("twig");
+(()=>{
+    try{
+        let Twig = require("twig"),
+            path = require('path'),
+            express = require('express'),
+            session = require('express-session'),
+            cookieParser = require('cookie-parser'),
+            logger = require('morgan'),
+            app = express(),
+            urls = require('./configuration/routes');
 
-        this.path = require('path');
-        this.Twig = Twig;
-        this.express = require('express');
-        this.session = require('express-session'); 
-        this.cookieParser = require('cookie-parser');
-        this.logger = require('morgan');
-        this.app = this.express();
-        this.urls = require('./configuration/routes');
-    }
+        //config project
+        const ROOT_PATH = path.resolve('./');
+        const WWW_PATH = ROOT_PATH + '/public';
+        const PORT = 3000;
 
-    run(){
-        try{
-            //config project
-            const ROOT_PATH = this.path.resolve('./');
-            const WWW_PATH = ROOT_PATH + '/public';
-            const PORT = 3000;
+        //config app object
+        app.set("twig options", {
+            allow_async: true, // Allow asynchronous compiling
+            strict_variables: false
+        });
 
-            //config app object
-            this.app.set("twig options", {
-                allow_async: true, // Allow asynchronous compiling
-                strict_variables: false
-            });
-
-            //bad bug twig extension (2 loops ??) !
-            let counter = 0;
-            this.Twig.extendFunction("url", (name, params)=> {
-                if(counter === 0){
-                    let urlCatch = this.urls.filter((url)=>{
-                        if(url.name === name){
-                            return url;
-                        }
-                    })[0];
-                    if(params === undefined){
-                        return urlCatch.path;
+        //bad bug twig extension (2 loops ??) !
+        let counter = 0;
+        Twig.extendFunction("url", (name, params)=> {
+            if(counter === 0){
+                let urlCatch = urls.filter((url)=>{
+                    if(url.name === name){
+                        return url;
                     }
-                    let pattern = /:([a-z0-9]+)/ig,
-                        keywords = urlCatch.path.match(pattern),
-                        len = keywords.length,
-                        url = urlCatch.path;
-                    for(let i = 0; i <  len; i++){
-                        let key = keywords[i].substring(1);
-                        url = url.replace(':' + key, params[key]);
-                    }
-                    return url;
+                })[0];
+                if(params === undefined){
+                    return urlCatch.path;
                 }
-                counter+= 1;
-            });
+                let pattern = /:([a-z0-9]+)/ig,
+                    keywords = urlCatch.path.match(pattern),
+                    len = keywords.length,
+                    url = urlCatch.path;
+                for(let i = 0; i <  len; i++){
+                    let key = keywords[i].substring(1);
+                    url = url.replace(':' + key, params[key]);
+                }
+                return url;
+            }
+            counter+= 1;
+        });
 
-            this.app.set("views", ROOT_PATH + '/source/views');
+        app.set("views", ROOT_PATH + '/source/views');
 
-            //middleware
-            this.app.use(this.logger('dev'));
-            this.app.use(this.express.json());
-            this.app.use(this.express.urlencoded({ extended: false }));
-            this.app.use(this.cookieParser());
-            this.app.use(this.express.static('public'));
+        //middleware
+        app.use(logger('dev'));
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: false }));
+        app.use(cookieParser());
+        app.use(express.static('public'));
 
-            //session
-            let sess = {
-                secret: 'keyboard cat',
-                cookie: {},
-                resave: false,
-                saveUninitialized: false,
+        //session
+        let sess = {
+            secret: 'keyboard cat',
+            cookie: {},
+            resave: false,
+            saveUninitialized: false,
+        };
+
+        if (app.get('env') && app.get('env') === 'production') {
+            app.set('trust proxy', 1) // trust first proxy
+            sess.cookie.secure = true // serve secure cookies
+            sess.saveUninitialized = true;
+        }
+        app.use(require('express-session')(sess));
+
+        // routes
+        let routes = express.Router(),
+            setRoute = (url, method, routes)=>{
+                url.view = require('./controllers/controllerFactory')(url.view);
+                switch(method.trim().toLowerCase()){
+                    case 'post':
+                        routes.post(url.path, url.view);
+                        break;
+                    case 'put':
+                        routes.put(url.path, url.view);
+                        break;
+                    case 'get':
+                        routes.get(url.path, url.view);
+                        break;
+                }
+                return routes;
             };
 
-            if (this.app.get('env') && this.app.get('env') === 'production') {
-                this.app.set('trust proxy', 1) // trust first proxy
-                sess.cookie.secure = true // serve secure cookies
-                sess.saveUninitialized = true;
-            }
-            this.app.use(require('express-session')(sess));
-
-            // routes
-            let routes = this.express.Router(),
-                setRoute = (url, method, routes)=>{
-                    url.view = require('./controllers/controllerFactory')(url.view);
-                    switch(method.trim().toLowerCase()){
-                        case 'post':
-                            routes.post(url.path, url.view);
-                            break;
-                        case 'put':
-                            routes.put(url.path, url.view);
-                            break;
-                        case 'get':
-                            routes.get(url.path, url.view);
-                            break;
-                    }
-                    return routes;
-                };
-
-            for(let i in this.urls){
-                let url = this.urls[ i ];
-                if(url.method.indexOf(',') !== -1){
-                    let methods = url.method.split(',')
-                    for(let j = 0; j < methods.length; j++){
-                        routes = setRoute(url,methods[j], routes);
-                    }
-                }else{
-                    routes = setRoute(url,url.method, routes);
+        for(let i in urls){
+            let url = urls[ i ];
+            if(url.method.indexOf(',') !== -1){
+                let methods = url.method.split(',')
+                for(let j = 0; j < methods.length; j++){
+                    routes = setRoute(url,methods[j], routes);
                 }
+            }else{
+                routes = setRoute(url,url.method, routes);
             }
-            this.app.use('/', routes);
-
-
-
-            // catch 404 and forward to error handler
-            this.app.use(function(req, res, next) {
-                return next(require('http-errors')(404, '404 not found'));
-            });
-
-            // error handler
-            this.app.use(function(err, req, res, next) {
-                // set locals, only providing error in development
-                res.locals.message = err.message;
-                //res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-                // render the error page
-                let statusErr = err.status || 500,
-                    view = "404";
-                res.status(statusErr);
-                if(statusErr >= 500){
-                    view = "500"
-                }
-                res.render(`errors/${view}.twig`);
-            });
-
-            //execute
-            this.app.listen(PORT, ()=>{
-                console.log(`Server is listening on port ${PORT}`);
-                console.log(`http://localhost:${PORT}`);
-            });
-        }catch(err){
-            console.error('error system ====> ', err);
         }
+        app.use('/', routes);
+
+        // catch 404 and forward to error handler
+        app.use(function(req, res, next) {
+            return next(require('http-errors')(404, '404 not found'));
+        });
+
+        // error handler
+        app.use(function(err, req, res, next) {
+            // set locals, only providing error in development
+            res.locals.message = err.message;
+            //res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+            // render the error page
+            let statusErr = err.status || 500,
+                view = "404";
+            res.status(statusErr);
+            if(statusErr >= 500){
+                view = "500"
+            }
+            res.render(`errors/${view}.twig`);
+        });
+
+        //execute
+        app.listen(PORT, ()=>{
+            console.log(`Welcome on kit 80 - node js framework`);
+            console.log(`Server is listening on port ${PORT}`);
+            console.log(`goto http://localhost:${PORT}`);
+            // setTimeout(()=>{
+            //     require('open')(`http://localhost:${PORT}`);
+            // }, 2000)
+        });
+    }catch(err){
+        console.error('error system ====> ', err);
     }
-}
-new Server().run();
+})();
