37141763648d82c0ae1fdb13a27cd4c8ca8e390d
niux3 <renaudbourdeau@gmail.com>
2026-08-20 17:32:51 +0200

    feat: upgrade dispatcher - no flash when change url

--- src/core/Dispatcher.js
+++ src/core/Dispatcher.js
@@ -19,12 +19,42 @@ export class Dispatcher {
         // 3. Intercepter globalement tous les clics sur les liens <a> internes
         document.addEventListener('click', (e) => {
             const link = e.target.closest('a')
+            if (!link) return
 
-            // On vérifie que c'est un lien interne (même origine)
-            if (link && link.origin === window.location.origin && !link.hasAttribute('data-native')) {
-                e.preventDefault()
-                this.navigateTo(link.getAttribute('href'))
+            const href = link.getAttribute('href') || ''
+
+            // 1. Gestion des ancres locales simples (ex: href="#lee")
+            if (href.startsWith('#')) {
+                // On laisse le comportement natif du navigateur pour défiler vers l'élément
+                // OU on gère le smooth scroll si l'élément existe dans le DOM
+                const targetEl = document.getElementById(href.substring(1))
+                if (targetEl) {
+                    e.preventDefault()
+                    targetEl.scrollIntoView({ behavior: 'smooth' })
+                    window.history.pushState(null, '', href)
+                }
+                return // On stoppe ici : ce n'est PAS un changement de page/route SPA
             }
+
+            // 2. Le reste de ton intercepteur SPA classique pour les vraies pages (/about, /project/1)
+            const isSameOrigin = link.origin === window.location.origin
+            const isTargetSelf = !link.target || link.target === '_self'
+            const isStandardClick = e.button === 0 && !e.ctrlKey && !e.metaKey && !e.shiftKey && !e.altKey
+
+            if (isSameOrigin && isTargetSelf && isStandardClick && !link.hasAttribute('data-native')) {
+                e.preventDefault()
+                this.navigateTo(link.pathname + link.search + link.hash)
+            }
+        })
+
+        document.addEventListener('submit', (e) => {
+            const form = e.target.closest('form')
+            console.log('form', form)
+            if (!form) return
+
+            e.preventDefault()
+            const formData = new FormData(form)
+            this.navigateTo(formData.get('action'))
         })
     }
 
@@ -40,10 +70,10 @@ export class Dispatcher {
             const route = this.router.getMatch()
             if (!route) return await this._errors(new Error('404'))
 
-            this._cleanup()
-
             const instance = await this._resolveController(route.controller)
             const view = await instance[route.action](...Object.values(route.params))
+
+            this._cleanup()
 
             this._render(view)
             this.activePage = instance
