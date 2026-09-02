bf40cbb0d38e898910adb43ff6a93bdd2a8b3d76
niux3 <renaudbourdeau@gmail.com>
2026-09-02 02:02:09 +0200

    fix: path to import configuration in configuration.test.js

--- tests/Configuration.test.js
+++ tests/Configuration.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect, beforeEach } from 'vitest'
-import { Configuration } from '../src/core/Configuration'
+import { Configuration } from '../src/Configuration'
 
 describe('Configuration', () => {
     beforeEach(() => {
